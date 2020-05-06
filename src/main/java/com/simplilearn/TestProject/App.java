c24f49639969ce72c6558b82cacbebadd5b06d52
Ubuntu <nithinnayak2min@nithinnayak2min.1ioye5xqdzqubdmktccd4vmb1d.gx.internal.cloudapp.net>
2020-05-06 08:27:40 +0000

    SecondCommit

--- src/main/java/com/simplilearn/TestProject/App.java
+++ src/main/java/com/simplilearn/TestProject/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Sum of 2 and 3 is : " + Math.add(2, 3));
     }
 }
---
+++ src/main/java/com/simplilearn/TestProject/Math.java
@@ -0,0 +1,7 @@
+package com.simplilearn.TestProject;
+
+public class Math {
+	public static int add(int a,int b) {
+		return a+b;
+	}
+}
